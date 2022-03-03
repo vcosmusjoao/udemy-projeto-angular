31a5073ac68f3373c8b150772b639deb1738e30a
João Victor Costa Santos <rm88372@fiap.com.br>
2022-03-03 11:01:27 -0300

    Criando modulo de clientes e criando componente de formulario de clientes.

---
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clientes-form',
+  templateUrl: './clientes-form.component.html',
+  styles: [
+  ]
+})
+export class ClientesFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
